45a0d578114e54ef86517800d4fb81265c16a0a7
Rick Rawson <rick.rawson@outlook.com>
2019-12-04 19:23:49 -0500

    2019-12-04 07:23 PM -- initial commit

---
+++ dir_stats.py
@@ -0,0 +1,14 @@
+"""
+dir_stats.py
+
+Richard E. Rawson
+2019-12-04
+
+Program Description:
+dir_stat is a disk usage statistics viewer and cleanup tool for Microsoft Windows (all current variants). dir_stat reads the whole directory tree once and then presents it in three useful views: the directory list, which resembles the tree view of the Windows Explorer but is sorted by file/subtree size; the treemap, which shows the whole contents of the directory tree straight away; and the extension list, which serves as a legend and shows statistics about the file types.
+"""
+
+import os
+
+if __name__ == '__main__':
+    pass
