afd01c27fd6a3e70607e61669766d86f072e6de0
Rick Rawson <rick.rawson@outlook.com>
2019-12-21 17:38:45 -0500

    Add readme.md and create remote repo

--- dir_stats.py
+++ dir_stats.py
@@ -5,7 +5,7 @@ Richard E. Rawson
 2019-12-04
 
 Program Description:
-dir_stats is a disk usage statistics viewer and cleanup tool for Microsoft Windows (all current variants). dir_stats reads the whole directory tree once and then presents it in three useful views: the directory list, which resembles the tree view of the Windows Explorer but is sorted by file/subtree size; the treemap, which shows the whole contents of the directory tree straight away; and the extension list, which serves as a legend and shows statistics about the file types.
+GOAL: dirStats is a disk usage statistics viewer and cleanup tool for Microsoft Windows (all current variants). dirStats reads the whole directory tree once and then presents it in three useful views: the directory list, which resembles the tree view of the Windows Explorer but is sorted by file/subtree size; the treemap, which shows the whole contents of the directory tree straight away; and the extension list, which serves as a legend and shows statistics about the file types.
 """
 
 import os
